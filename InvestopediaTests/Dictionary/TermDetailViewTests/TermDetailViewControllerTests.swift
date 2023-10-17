ff31208e7121ed7d5517aed4882ac2a369045397
Joao Ponte <joao_ponte@msn.com>
2023-10-17 11:35:14 +0100

    Add tests to my TermDetailViewController

---
+++ InvestopediaTests/Dictionary/TermDetailViewTests/TermDetailViewControllerTests.swift
@@ -0,0 +1,53 @@
+//
+//  TermDetailViewControllerTests.swift
+//  InvestopediaTests
+//
+//  Created by João Ponte on 17/10/2023.
+//
+
+import XCTest
+@testable import Investopedia
+
+class TermDetailViewControllerTests: XCTestCase {
+    var viewController: TermDetailViewController!
+    var viewModelMock: TermDetailViewModelMock!
+
+    override func setUp() {
+        super.setUp()
+        let storyboard = UIStoryboard(name: "Main", bundle: nil)
+        viewController = storyboard.instantiateViewController(withIdentifier: "TermDetailViewController") as? TermDetailViewController
+        viewModelMock = TermDetailViewModelMock()
+        viewController.viewModel = viewModelMock
+        viewController.loadViewIfNeeded()
+    }
+
+    // MARK: - Tests for IBActions
+
+    func testPreviousButtonTapped() {
+
+        // When
+        viewController.previousButtonTapped(UIButton())
+
+        // Then
+        XCTAssertTrue(viewModelMock.selectPreviousTermCalled)
+    }
+
+    func testNextButtonTapped() {
+        // When
+        viewController.nextButtonTapped(UIButton())
+
+        // Then
+        XCTAssertTrue(viewModelMock.selectNextTermCalled)
+        // Add more assertions as needed based on the expected behavior
+    }
+
+    // MARK: - Other Tests for the ViewController
+
+    override func tearDown() {
+        viewController = nil
+        viewModelMock = nil
+        super.tearDown()
+    }
+}
+
+
