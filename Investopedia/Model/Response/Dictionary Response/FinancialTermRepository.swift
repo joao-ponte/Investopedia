0fdf16fcd13c8126c7a0ae4f8d0bbfd81bc9675a
Joao Ponte <joao_ponte@msn.com>
2023-10-28 13:19:11 +0100

    Remove comments. Add new file to handle JSONFinancialTerm Create a Protocol Update DictionaryViewModel.

---
+++ Investopedia/Model/Response/Dictionary Response/FinancialTermRepository.swift
@@ -0,0 +1,8 @@
+//
+//  FinancialTermRepository.swift
+//  Investopedia
+//
+//  Created by João Ponte on 28/10/2023.
+//
+
+import Foundation
---
+++ Investopedia/Model/Response/Dictionary Response/JSONFinancialTermRepository.swift
@@ -0,0 +1,8 @@
+//
+//  JSONFinancialTermRepository.swift
+//  Investopedia
+//
+//  Created by João Ponte on 28/10/2023.
+//
+
+import Foundation
